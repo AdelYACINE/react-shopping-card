4d7e987d05cd4f46b96a78ffbc5d7b420fc46926
AdelYACINE <ahmed95yacine@gmail.com>
2023-10-22 14:41:24 +0200

    add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Cart from "./component/cart/Cart";
+//import Cart from "./component/cart/Cart";
 import Footer from "./component/footer/Footer";
-import Home from "./component/home/Home";
+//import Home from "./component/home/Home";
 import NavBar from "./component/navbar/NavBar";
-import Products from "./component/product/Products";
-import ProductDetails from "./component/productDetails/ProductDetails";
+//import Products from "./component/product/Products";
+//import ProductDetails from "./component/productDetails/ProductDetails";
 import ContextProductProvider from "./context/contextproduct/ContextProductProvider";
+import { Suspense, lazy } from "react";
+import Loader from "./component/loader/Loader";
+
+const Home = lazy(() => import("./component/home/Home"));
+const Products = lazy(() => import("./component/product/Products"));
+const ProductDetails = lazy(() =>
+  import("./component/productDetails/ProductDetails")
+);
+const Cart = lazy(() => import("./component/cart/Cart"));
 
 function App() {
   return (
     <ContextProductProvider>
       <Router>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Suspense>
+
         <Footer />
       </Router>
     </ContextProductProvider>
---
+++ src/component/loader/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <h1 className="loader-text">loading...</h1>
+    </div>
+  );
+};
+
+export default Loader;
